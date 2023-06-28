4e9c36d076509869bd21b9670ba7f04f990771b6
Seannio <Sean.Morris@iot-ex.co.jp>
2023-06-28 15:48:19 +0900

    added *

--- Sumer/Sumer/settings.py
+++ Sumer/Sumer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-d8zf2t=p&pwtqy2qs2-!y&dvf)n1778ztg22@$3af%qgcoh0sz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =['*'] 
 
 
 # Application definition
